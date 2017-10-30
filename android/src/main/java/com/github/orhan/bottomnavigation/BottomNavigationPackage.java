0e900f252a8568313dfdaecc55d97b4e52ed4f72
Orhan Sönmez <orhnsnmz@gmail.com>
2017-10-30 15:06:53 +0100

    Updated Android files to be compatible with RN 0.48+

--- android/src/main/java/com/github/orhan/bottomnavigation/BottomNavigationPackage.java
+++ android/src/main/java/com/github/orhan/bottomnavigation/BottomNavigationPackage.java
@@ -20,7 +20,6 @@ public class BottomNavigationPackage implements ReactPackage {
         return Collections.emptyList();
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
